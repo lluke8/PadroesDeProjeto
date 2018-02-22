d1d95afbee473ae2a23f033bd1facfc707b06754
Lucas Dantas <luk.dantas@gmail.com>
2018-02-22 17:56:11 -0300

    inserindo mais robots e humans!

--- MediatorExercise/src/exercicio/br/ifpb/Teste.java
+++ MediatorExercise/src/exercicio/br/ifpb/Teste.java
@@ -6,13 +6,19 @@ public class Teste {
 		
 		//participantes
 		HumanParticipant human = new HumanParticipant(chatroomimpl);
+		HumanParticipant joao = new HumanParticipant(chatroomimpl);
 		Robot robot = new Robot(chatroomimpl);
+		Robot robocop = new Robot(chatroomimpl);
 		
+		chatroomimpl.addParticipant(joao);
 		chatroomimpl.addParticipant(human);
 		chatroomimpl.addParticipant(robot);
+		chatroomimpl.addParticipant(robocop);
 		
 		human.send("Ola, testando!");
 		robot.send("Hello, testing!");
+		robocop.send("Hello from robocop!");
+		joao.send("Hello from joao!");
 		
 	}
 }
